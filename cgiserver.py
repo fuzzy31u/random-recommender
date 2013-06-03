4bf6cb50957080b38dab3837be44ffd7906d55a3
kamiyaU <fuzzy31u@gmail.com>
2013-06-04 00:18:25 +0900

    Enable server start on 80 port.

--- cgiserver.py
+++ cgiserver.py
@@ -1,2 +1,4 @@
-import CGIHTTPServer
-CGIHTTPServer.test()
+import cgi
+import BaseHTTPServer,CGIHTTPServer
+
+BaseHTTPServer.HTTPServer(('127.0.0.1', 80), CGIHTTPServer.CGIHTTPRequestHandler ).serve_forever()
